00141175249fc9da72684fbb2109b5266a4349f2
Caximorris <mjtamagni@hotmail.com>
2024-10-24 17:18:59 +0200

    fixed body

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ exports.handler = async (event) => {
     const name = event.queryStringParameters && event.queryStringParameters.name ? event.queryStringParameters.name : 'World';
     const currentTime = new Date().toLocaleTimeString('en-US', { hour12: false });
 
-    return `<html><body><h1>Hello ${name}, the time is ${currentTime}</h1></body></html>`;
+    return `Hello ${name}, the time is ${currentTime}`;
 };
