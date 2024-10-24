a5693b9fd71478b1338cd3d58b5c44f0b0d4c66f
Caximorris <mjtamagni@hotmail.com>
2024-10-24 17:17:36 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -2,15 +2,7 @@
 
 exports.handler = async (event) => {
     const name = event.queryStringParameters && event.queryStringParameters.name ? event.queryStringParameters.name : 'World';
-    const currentTime = new Date().toISOString();
+    const currentTime = new Date().toLocaleTimeString('en-US', { hour12: false });
 
-    const response = {
-        statusCode: 200,
-        body: `<html><body><h1>Hello ${name}, the time is ${currentTime}</h1></body></html>`,
-        headers: {
-            "Content-Type": "text/html"
-        },
-    };
-
-    return response;
+    return `<html><body><h1>Hello ${name}, the time is ${currentTime}</h1></body></html>`;
 };
