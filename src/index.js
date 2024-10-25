3b0668bb71e71d5e2c47213082faf13436d2ce5e
Mati <88194082+Caximorris@users.noreply.github.com>
2024-10-25 10:36:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 exports.handler = async (event) => {
     const name = event.queryStringParameters && event.queryStringParameters.name ? event.queryStringParameters.name : 'World';
-    const currentTime = new Date().toLocaleTimeString('en-US', { hour12: false });
+    const currentTime = new Date().toLocaleTimeString('en-US', { hour12: false, timeZone: 'Europe/Madrid' });
 
     return `Hello ${name}, the time is ${currentTime}`;
 };
